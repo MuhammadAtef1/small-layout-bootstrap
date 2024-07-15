3a9ceaaa5cf35487c6bf4843d4f3f5cfd64badab
MuhammadAtef1 <muhammadatef101@gmail.com>
2024-07-15 15:31:36 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,14 +2,34 @@ let btn = document.querySelector(".dark-mode-btn");
 let html = document.querySelector("html");
 let ctr = 0 ;
 
-console.log(btn);
+let fonts = document.querySelectorAll(".nav-fonts>i");
 
 btn.addEventListener("click",function() {
     if (ctr % 2 == 0) {
         html.setAttribute("data-bs-theme","dark");
+        [...fonts].forEach(element => {
+            element.addEventListener("mouseover",function(){
+                        element.style.color = "white";
+                    })
+        });
+        [...fonts].forEach(element => {
+            element.addEventListener("mouseleave",function(){
+                element.style.color = "black" ;
+            })
+        });
     }
     else if (ctr%2 != 0) {
         html.removeAttribute("data-bs-theme");
+        [...fonts].forEach(element => {
+            element.addEventListener("mouseover",function(){
+                        element.style.color = "black";
+                    })
+        });
+        [...fonts].forEach(element => {
+            element.addEventListener("mouseleave",function(){
+                element.style.color = "rgba(0, 0, 0, 0.651)" ;
+            })
+        });
     }
     ++ctr;
 })
